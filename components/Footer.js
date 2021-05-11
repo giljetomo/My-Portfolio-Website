13ac2ab476f0ddfc56b0ed7049b3deabf329a885
Macbook Pro <gmjetomo@outlook.com>
2021-05-10 20:08:58 -0700

    feat: footer-social icons/redirects

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,9 @@
+import IconButton from '@material-ui/core/IconButton';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import EmailTwoToneIcon from '@material-ui/icons/EmailTwoTone';
 import { makeStyles } from '@material-ui/core/styles';
+import SendMail from  '../components/SendMail'
 
 const useStyles = makeStyles(theme => ({
   clickableIcon: {
@@ -16,10 +19,21 @@ const Footer = () => {
 
   return (
     <footer>
-      <GitHubIcon onClick={event => window.open('https://github.com/giljetomo')}
-      className={classes.clickableIcon}
-       />
-      <LinkedInIcon />
+      <IconButton
+        color="primary"
+      >
+        <GitHubIcon onClick={event => window.open('https://github.com/giljetomo')} />
+       </IconButton>
+      <IconButton
+      color="primary"
+      >
+        <LinkedInIcon onClick={event => window.open('https://www.linkedin.com/in/gmjetomo')} />
+      </IconButton>
+      <IconButton
+        color="primary"
+      >
+        <EmailTwoToneIcon onClick={SendMail}/>
+      </IconButton>
     </footer>
   );
 }
---
+++ components/SendMail.js
@@ -0,0 +1,6 @@
+const SendMail = () => {
+    const mailto = "mailto:gmjetomo@outlook.com?subject=Send Me An Email&body=Body content";
+    window.location.href = mailto;
+};
+
+export default SendMail;
