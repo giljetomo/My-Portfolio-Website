1ba4e951c5f4dd349c4bf8414225b08f33431a80
Macbook Pro <gmjetomo@outlook.com>
2021-05-13 18:05:08 -0700

    feat: added contact details

--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,8 @@
 import Head from 'next/head'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import { Grid } from '@material-ui/core';
 
 const Contact = () => {
   return (
@@ -7,10 +11,31 @@ const Contact = () => {
       <title>Gil's Contact</title>
     </Head>
       <div className="contact">
-        <h2>Contact</h2>
+        <Grid
+          container
+          justify="center"
+          >
+          <Card
+            style={{
+            width: 450,
+            height: 200,
+            background: 'linear-gradient(to right, #6999d8 6%, #fafafa 100%)'
+            }}>
+            <CardHeader
+            title="Gil Jetomo"
+            subheader="Software Developer - iOS"
+            >
+            </CardHeader>
+            <CardContent>
+                gmjetomo@outlook.com<br />
+                (778) 721-8808
+            </CardContent>
+          </Card>
+        </Grid>
       </div>
     </>
    );
 }
  
 export default Contact;
+
