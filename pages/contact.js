a8db8cc40b757dbf54c3bd72b49a01d9cf8cbeec
Macbook Pro <gmjetomo@outlook.com>
2021-05-13 22:16:54 -0700

    refactor: styling

--- pages/contact.js
+++ pages/contact.js
@@ -3,36 +3,47 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
 import { Grid } from '@material-ui/core';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Play'
+    ].join(','),
+  },});
 
 const Contact = () => {
   return (
     <>
-    <Head>
-      <title>Gil's Contact</title>
-    </Head>
-      <div className="contact">
-        <Grid
-          container
-          justify="center"
-          >
-          <Card
-            style={{
-            width: 400,
-            height: 200,
-            background: 'linear-gradient(to right, #6999d8 6%, #fafafa 100%)'
-            }}>
-            <CardHeader
-            title="Gil Jetomo"
-            subheader="Software Developer - iOS"
+    <ThemeProvider theme={theme}>
+      <Head>
+        <title>Gil's Contact</title>
+      </Head>
+        <div>
+          <Grid
+            container
+            justify="center"
             >
-            </CardHeader>
-            <CardContent>
-                gmjetomo@outlook.com<br />
-                (778) 721-8808
-            </CardContent>
-          </Card>
-        </Grid>
-      </div>
+            <Card
+              style={{
+              width: 400,
+              height: 200,
+              background: 'linear-gradient(to right, #6999d8 6%, #fafafa 100%)'
+              }}>
+              <CardHeader 
+              title="Gil Jetomo"
+              subheader="Software Developer - iOS"
+              >
+              </CardHeader>
+              <CardContent className="contact">
+                  <br />
+                  gmjetomo@outlook.com<br />
+                  (778) 721-8808
+              </CardContent>
+            </Card>
+          </Grid>
+        </div>
+      </ThemeProvider>
     </>
    );
 }
